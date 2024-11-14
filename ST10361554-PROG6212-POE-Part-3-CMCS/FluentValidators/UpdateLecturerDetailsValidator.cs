4a5b6c28df1c343d5b42fe6f83b9add538bf74c3
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-15 00:26:42 +0200

    Add UpdateLecturerDetailsValidator for user role validation
    Introduce UpdateLecturerDetailsValidator in the ST10361554_PROG6212_POE_Part_3_CMCS.FluentValidators namespace to validate UpdateLecturerUserViewModel using FluentValidation. Enforce common rules for all users, such as non-empty first name, surname, email, and phone number fields. Apply specific validation rules based on user roles: restrict "Lecturer" role to updating Faculty and Module fields only, while allowing "Academic Manager" and "HR" roles to update all fields. Includes reference to FluentValidation documentation.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/FluentValidators/UpdateLecturerDetailsValidator.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation;
+using ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.FluentValidators
+{
+    // Code Attribution:
+    // Creating your first validator
+    // Fluentvalidation.net
+    // 14 November 2024
+    // https://docs.fluentvalidation.net/en/latest/start.html
+
+    public class UpdateLecturerDetailsValidator : AbstractValidator<UpdateLecturerUserViewModel>
+    {
+        public UpdateLecturerDetailsValidator(string userRole)
+        {
+            // Common rules for all users
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname is required.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("A valid email is required.");
+
+            // For PhoneNumber: Remove all whitespaces temporarily for validation
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Must(phone => !string.IsNullOrWhiteSpace(phone) && phone.Replace(" ", "").Length <= 15)
+                .WithMessage("Phone number cannot be longer than 15 characters");
+
+            if (userRole.Equals("Lecturer"))
+            {
+                // Lecturer can only update specific fields
+                RuleFor(x => x.Faculty).NotEmpty().WithMessage("Faculty is required.");
+                RuleFor(x => x.Module).NotEmpty().WithMessage("Module is required.");
+
+                // Prevent update of address and banking info for Lecturer
+                RuleFor(x => x.AccountNumber).Empty().WithMessage("You cannot update banking information.");
+                RuleFor(x => x.BankName).Empty().WithMessage("You cannot update banking information.");
+                RuleFor(x => x.BranchCode).Empty().WithMessage("You cannot update banking information.");
+                RuleFor(x => x.StreetAddress).Empty().WithMessage("You cannot update your address.");
+                RuleFor(x => x.AreaAddress).Empty().WithMessage("You cannot update your address.");
+                RuleFor(x => x.City).Empty().WithMessage("You cannot update your address.");
+                RuleFor(x => x.Province).Empty().WithMessage("You cannot update your address.");
+            }
+            else if (userRole.Equals("Academic Manager") || userRole.Equals("HR"))
+            {
+                // Academic Manager and HR can update all fields
+                RuleFor(x => x.Faculty).NotEmpty().WithMessage("Faculty is required.");
+                RuleFor(x => x.Module).NotEmpty().WithMessage("Module is required.");
+
+                RuleFor(x => x.AccountNumber).NotEmpty().WithMessage("Account number is required.");
+                RuleFor(x => x.BankName).NotEmpty().WithMessage("Bank name is required.");
+                RuleFor(x => x.BranchCode).NotEmpty().WithMessage("Branch code is required.");
+
+                RuleFor(x => x.StreetAddress).NotEmpty().WithMessage("Street address is required.");
+                RuleFor(x => x.AreaAddress).NotEmpty().WithMessage("Area address is required.");
+                RuleFor(x => x.City).NotEmpty().WithMessage("City is required.");
+                RuleFor(x => x.Province).NotEmpty().WithMessage("Province is required.");
+            }
+        }
+    }
+}
