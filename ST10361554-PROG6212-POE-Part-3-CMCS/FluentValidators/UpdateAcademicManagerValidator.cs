0a9a6f4f2c99bf488d5721cb2c220c90e7a81c44
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-15 00:37:00 +0200

    Add UpdateAcademicManagerValidator for model validation
    Introduce UpdateAcademicManagerValidator class in the
    ST10361554_PROG6212_POE_Part_3_CMCS.FluentValidators namespace.
    This class uses FluentValidation to validate the
    UpdateAcademicManagerViewModel model. Validation rules ensure
    FirstName, Surname, Email, PhoneNumber, StreetAddress, AreaAddress,
    City, and Province fields are not empty. Additionally, the Email
    field must contain a valid email address, and the PhoneNumber field
    must not exceed 15 characters when whitespaces are removed.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/FluentValidators/UpdateAcademicManagerValidator.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.FluentValidators
+{
+    // Code Attribution:
+    // Creating your first validator
+    // Fluentvalidation.net
+    // 14 November 2024
+    // https://docs.fluentvalidation.net/en/latest/start.html
+
+    public class UpdateAcademicManagerValidator : AbstractValidator<UpdateAcademicManagerViewModel>
+    {
+        public UpdateAcademicManagerValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname is required.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("A valid email is required.");
+
+            // For PhoneNumber: Remove all whitespaces temporarily for validation
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty().WithMessage("Phone number is required.")
+                .Must(phone => !string.IsNullOrWhiteSpace(phone) && phone.Replace(" ", "").Length <= 15)
+                .WithMessage("Phone number cannot be longer than 15 characters");
+
+            RuleFor(x => x.StreetAddress).NotEmpty().WithMessage("Street address is required.");
+            RuleFor(x => x.AreaAddress).NotEmpty().WithMessage("Area address is required.");
+            RuleFor(x => x.City).NotEmpty().WithMessage("City is required.");
+            RuleFor(x => x.Province).NotEmpty().WithMessage("Province is required.");
+        }
+    }
+}
