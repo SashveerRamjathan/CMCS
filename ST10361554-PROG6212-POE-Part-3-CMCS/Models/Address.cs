9154ffd61ecd496cdb73398e7b1da1fae3a1beba
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-20 21:37:18 +0200

    Add Address class with properties and [NotMapped] attribute
    Introduced a new `Address` class in the `ST10361554_PROG6212_POE_Part_3_CMCS.Models` namespace. This class includes properties for `Name`, `Street`, `Area`, `City`, `Province`, `PhoneNumber`, and `Email`. The class is marked with the `[NotMapped]` attribute, indicating it should not be mapped to a database table.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/Models/Address.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.Models
+{
+    // Class representing an address
+    [NotMapped]
+    public class Address
+    {
+        public string Name { get; set; } // Name of the person or entity
+        public string Street { get; set; } // Street address
+        public string Area { get; set; } // Area
+        public string City { get; set; } // City
+        public string Province { get; set; } // Province
+        public string PhoneNumber { get; set; } // Contact phone number
+        public string Email { get; set; } // Contact email address
+    }
+}
