8539df6de9990016a6ff5b30436a375d097ea02e
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-21 01:27:11 +0200

    Add InvoiceItemViewModel class to ViewModels namespace
    Introduced a new namespace `ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels` and added the `InvoiceItemViewModel` class within it. This class includes two public properties: `Invoice` and `Claim`, which are of types defined in the `ST10361554_PROG6212_POE_Part_3_CMCS.Models` namespace. Also added a using directive for the models namespace.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/ViewModels/InvoiceItemViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ST10361554_PROG6212_POE_Part_3_CMCS.Models;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels
+{
+    // represents the view model for an invoice item containing an invoice and the associated claim
+    public class InvoiceItemViewModel
+    {
+        public Invoice Invoice { get; set; }
+
+        public Claim Claim { get; set; }
+    }
+}
