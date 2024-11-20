7d5a91303d44ac6f7311d6dc070658ba0cc0ceba
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-21 01:27:30 +0200

    Add GenerateInvoiceViewModel for invoice generation
    Introduced a new namespace `ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels` and added a new class `GenerateInvoiceViewModel` within it. This class includes two public properties: `Claims`, a list of `Claim` objects, and `InvoiceItems`, a list of `InvoiceItemViewModel` objects. Also added a using directive for the `ST10361554_PROG6212_POE_Part_3_CMCS.Models` namespace.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/ViewModels/GenerateInvoiceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ST10361554_PROG6212_POE_Part_3_CMCS.Models;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels
+{
+    public class GenerateInvoiceViewModel
+    {
+        public List<Claim> Claims { get; set; } // List of claims to generate invoices for
+
+        public List<InvoiceItemViewModel> InvoiceItems { get; set; } // List of invoices generated
+    }
+}
