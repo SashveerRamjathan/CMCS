71eecd295eb52636a6cdec6f81bde877e423c36b
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-20 21:53:10 +0200

    Add InvoiceModel class and logo image for invoices
    Updated InvoiceModel.cs to include the InvoiceModel class in the ST10361554_PROG6212_POE_Part_3_CMCS.Models namespace. This class includes properties for InvoiceNumber, IssueDate, LecturerAddress, CompanyAddress, LecturerClaim, Comments, and LogoImage, and is marked with the [NotMapped] attribute. Added claimInvoiceLogo.jpg as the logo image for invoices.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/Models/InvoiceModel.cs
@@ -0,0 +1,22 @@
+﻿using QuestPDF.Infrastructure;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.Models
+{
+    // Class representing the model for an invoice
+    [NotMapped]
+    public class InvoiceModel
+    {
+        public int InvoiceNumber { get; set; } // Unique number identifying the invoice
+        public DateTime IssueDate { get; set; } // Date when the invoice was issued
+
+        public Address LecturerAddress { get; set; } // Address of the Lecturer the invoice is issued to
+        public Address CompanyAddress { get; set; } // Address of the company issuing the invoice
+
+        public Claim LecturerClaim { get; set; } // Claim details for the lecturer
+
+        public string Comments { get; set; } // Additional comments or notes for the invoice
+
+        public Image LogoImage { get; set; } // Claim logo image to be included in the invoice
+    }
+}
