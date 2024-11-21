5f8999a29b2bb6ebaa625783392cbbf79927c9fa
Sashveer Ramjathan <ST10361554@vcconnect.edu.za>
2024-11-22 01:23:49 +0200

    Add ReportIndexViewModel for managing report data
    Introduced a new namespace `ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels`.
    Created a new class `ReportIndexViewModel` within this namespace.
    Added properties `Months`, `Modules`, and `Reports` to the class.
    Added a using directive for the `ST10361554_PROG6212_POE_Part_3_CMCS.Models` namespace.
    This change supports the display and management of report data in the UI.

---
+++ ST10361554-PROG6212-POE-Part-3-CMCS/ViewModels/ReportIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ST10361554_PROG6212_POE_Part_3_CMCS.Models;
+
+namespace ST10361554_PROG6212_POE_Part_3_CMCS.ViewModels
+{
+    public class ReportIndexViewModel
+    {
+        public List<string> Months { get; set; } = new List<string>(); // Months in "MMM yyyy" format
+
+        public List<string> Modules { get; set; } = new List<string>(); // List of distinct modules
+
+        public List<Report> Reports { get; set; } = new List<Report>(); // List of reports generated
+    }
+}
